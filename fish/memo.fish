a6ecc71cb110630f0d5cd799c7d11c26c46529f9
Kumac13 <foohilack@gmail.com>
2021-01-07 16:32:13 +0900

    add fishfile

---
+++ fish/memo.fish
@@ -0,0 +1,45 @@
+# vim用のメモを取るためのスクリプト
+function memo
+
+   set -e date
+   setenv date (date +%F) # フルの日付
+   setenv yearAndMonth (date +%Y-%m) # フォルダ用の月までの日付
+
+   # 引数がない場合は，その日付のファイルを作成する 
+   if count $argv > /dev/null
+      if test -d ~/Dropbox/memo/$yearAndMonth
+      echo  "$argvを作成しました"
+         if test -f ~/Dropbox/memo/$yearAndMonth/$argv.md
+             vim  ~/Dropbox/memo/$yearAndMonth/$argv.md
+         else
+             echo \# memo $date >> ~/dropbox/memo/$yearAndMonth/$argv.md
+             vim  ~/dropbox/memo/$yearAndMonth/$argv.md
+         end
+      else
+         mkdir ~/Dropbox/memo/$yearAndMonth
+         echo \# memo $date >> ~/dropbox/memo/$yearAndMonth/$argv.md
+         vim  ~/dropbox/memo/$yearAndMonth/$argv.md
+      end
+   # 引数があるときは，その名前のファイルを作成する
+   else    
+      if test -d ~/Dropbox/memo/$yearAndMonth
+         if test -f ~/Dropbox/memo/$yearAndMonth/$date.md 
+             vim  ~/Dropbox/memo/$yearAndMonth/$date.md
+         else 
+             echo \# memo $date >> ~/dropbox/memo/$yearAndMonth/$date.md
+             vim  ~/dropbox/memo/$yearAndMonth/$date.md
+         end
+      else
+         mkdir ~/Dropbox/memo/$yearAndMonth
+         echo \# memo $date >> ~/dropbox/memo/$yearAndMonth/$date.md
+         vim  ~/dropbox/memo/$yearAndMonth/$date.md
+      end
+
+   end
+ end
+
+function gmemo
+   set -e date
+   setenv yearAndMonth (date +%Y-%m)
+   cd ~/Dropbox/memo/$yearAndMonth
+end
---
+++ fish/memo_for_fish.md
@@ -0,0 +1,31 @@
+# intallation of fish
+```sh
+brew install fish
+````
+
+# installation of fihser
+```sh
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+```
+
+# installation of plugins
+```sh
+fisher install oh-my-fish/theme-bobthefish
+
+fisher install jethrokuan/z
+
+fisher install jethrokuan/fzf
+
+fisher install decors/fish-ghq
+```
+
+# auto complete
+```sh
+fish_update_completions
+```
+
+# change 'ls' command color 
+```sh
+touch ~/.config/fish/config.fish
+export LSCOLORS=gxfxcxdxbxegedabagacad
+```
