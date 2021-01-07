ffe56a730d1f9f28f09352ddfc1b26b2fc5076fa
Kumac13 <foohilack@gmail.com>
2021-01-07 15:19:46 +0900

    月別のフォルダ構成にしてその配下にメモを入れるようにした

--- fish/functions/memo.fish
+++ fish/functions/memo.fish
@@ -3,25 +3,36 @@
 function memo
 
    set -e date
-   setenv date (date +%F)
+   setenv date (date +%F) # フルの日付
+   setenv yearAndMonth (date +%Y-%m) # フォルダ用の月までの日付
 
    # 引数がない場合は，その日付のファイルを作成する 
    if test -d $argv
-      if test -f ~/dropbox/bear/$date.md 
-          vim  ~/dropbox/bear/$date.md
-      else 
-          echo \# memo $date >> ~/dropbox/bear/$date.md
-          vim  ~/dropbox/bear/$date.md
+      if test -d ~/Dropbox/memo/$yearAndMonth
+         if test -f ~/Dropbox/memo/$yearAndMonth/$date.md 
+             vim  ~/Dropbox/memo/$yearAndMonth/$date.md
+         else 
+             echo \# memo $date >> ~/dropbox/memo/$yearAndMonth/$date.md
+             vim  ~/dropbox/memo/$yearAndMonth/$date.md
+         end
+      else
+         mkdir ~/Dropbox/memo/$yearAndMonth
+         echo \# memo $date >> ~/dropbox/memo/$yearAndMonth/$date.md
+         vim  ~/dropbox/memo/$yearAndMonth/$date.md
       end
    # 引数があるときは，その名前のファイルを作成する
-   else
-      if test -f ~/dropbox/bear/$argv.md 
-          echo \# memo $date >> ~/dropbox/bear/$argv.md
-          vim  ~/dropbox/bear/$argv.md
-      else 
-          echo \# memo $date >> ~/dropbox/bear/$argv.md
-          vim  ~/dropbox/bear/$argv.md
+   else    
+      if test -d ~/Dropbox/memo/$yearAndMonth
+         if test -f ~/Dropbox/memo/$yearAndMonth/$date.md 
+             vim  ~/Dropbox/memo/$yearAndMonth/$date.md
+         else 
+             echo \# memo $date >> ~/dropbox/memo/$yearAndMonth/$date.md
+             vim  ~/dropbox/memo/$yearAndMonth/$date.md
+         end
+      else
+         mkdir ~/Dropbox/memo/$yearAndMonth
+         echo \# memo $date >> ~/dropbox/memo/$yearAndMonth/$date.md
+         vim  ~/dropbox/memo/$yearAndMonth/$date.md
       end
    end
- 
  end
