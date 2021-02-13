e870bdde1991076ee69f8b781a9249d4b3d9eeaa
Hiraku Kumagai <kumac13@Hirakus-MacBook-Pro-13.local>
2021-02-13 20:46:08 +0900

    Add path for postgres

--- config.fish
+++ config.fish
@@ -1,20 +1,11 @@
 # env
 export LSCOLORS=gxfxcxdxbxegedabagacad
-set -x FZF_DEFAULT_OPTS "--layout=reverse --inline-info --exit-0 -m"
-set -x FZF_DEFAULT_COMMAND "ag -g ."
-set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
 set -x LANG "en_US.UTF-8"
-set -x LC_CTYPE "en_US.UTF-8"
-
 
 # alias
 alias prev="fzf --preview 'bat --style=numbers --color=always {}'"
-alias gs='git status'
-alias gd='git diff'
-alias gl='git log'
-alias gc='git checkout'
 
 # path
 set -x PATH $HOME/.pyenv/bin $PATH
-. (pyenv init - | psub)
+set -x PGDATA /user/local/var/postgres
 
