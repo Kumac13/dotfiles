301367092ac767b831a0ef345962fc652aca9806
Hiraku Kumagai <kumac13@Hirakus-MacBook-Pro-13.local>
2021-02-15 09:12:16 +0900

    Fix fish_user_key_bindings for default fish key bindings

--- fish/fish_user_key_bindings.fish
+++ fish/fish_user_key_bindings.fish
@@ -1,3 +1,7 @@
 function fish_user_key_bindings
+		for mode in insert default visual
+    	fish_default_key_bindings -M $mode
+  	end
+  	fish_vi_key_bindings --no-erase
     bind -M insert jj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 end
