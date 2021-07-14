b781ffe6db15810a90c59b033abbf8016bf05d75
Kumac13 <foohilack@gmail.com>
2021-07-14 15:09:16 +0900

    Add key binding for fish

--- fish/fish_user_key_bindings.fish
+++ fish/fish_user_key_bindings.fish
@@ -5,3 +5,5 @@ function fish_user_key_bindings
   	fish_vi_key_bindings --no-erase
     bind -M insert jj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 end
+
+fzf_key_bindings
