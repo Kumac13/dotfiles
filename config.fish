b260f3713bdefda84bb1a2f4a2ed6fa6c10be03c
kumac13 <foohilack@gmail.com>
2023-06-14 08:00:48 +0900

    set locale

--- config.fish
+++ config.fish
@@ -2,6 +2,9 @@
 export LSCOLORS=gxfxcxdxbxegedabagacad
 set -x LANG "en_US.UTF-8"
 set -x LC_CTYPE "en_US.UTF-8"
+set -x LC_MESSAGES "en_US.UTF-8"
+set -x LC_ALL "en_US.UTF-8"
+
 fish_vi_key_bindings
 
 # alias
