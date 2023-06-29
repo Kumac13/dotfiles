a60ac46c7a7d7ffa5c48657195b24fb8fe64f850
kumac13 <foohilack@gmail.com>
2023-06-29 09:50:15 +0900

    feat: set path setting for WSL2

--- config.fish
+++ config.fish
@@ -13,8 +13,23 @@ alias postgrestart='pg_ctl -D /usr/local/var/postgres start'
 alias wnt='cd ~/project/'
 alias vi='vim'
 
+if uname -r | grep 'micro' >/dev/null
+  set -x PATH $HOME/.yarn/bin $PATH
+  set -x PATH $HOME/.sdk/bin $PATH
+  set -x PATH /home/linuxbrew/.linuxbrew/bin $PATH
+
+  alias explorer.exe='/mnt/c/Windows/explorer.exe'
+
+  if status --is-interactive
+    status --is-interactive; and source (jenv init - --no-rehash fish | psub)
+  end
+
+  if pwd | grep 'mnt' >/dev/null
+    cd $HOME
+  end
+end
+
 # path
-# set RUBY_VERSION (ruby -v | sed 's/ruby ([0-9]\.[0-9]\.[0-9]).*/\1/')
 set -x PATH /usr/local/bin/python3 /usr/local/bin /usr/sbin $PATH
 set -x PGDATA /usr/local/var/postgres
 set -x PATH $HOME/.rbenv/bin $PATH
@@ -25,10 +40,3 @@ set -x PATH $HOME/.cargo/bin $PATH
 eval (nodenv init - | source)
 rbenv init - fish | source
 status --is-interactive; and source (rbenv init -|psub)
-
-if uname -r | grep 'micro' >/dev/null
-  if status --is-interactive
-    status --is-command-substitution; or source (jenv init -|psub)
-  end
-  cd $HOME
-end
