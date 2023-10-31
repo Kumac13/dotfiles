cd35a9ed40a5002c2cd972abf9a2c953acc56336
Kumac13 <foohilack@gmail.com>
2023-10-31 17:21:14 +0900

    chore: modify config for fish

--- config.fish
+++ config.fish
@@ -12,6 +12,7 @@ alias prev="fzf --preview 'bat --style=numbers --color=always {}'"
 alias postgrestart='pg_ctl -D /usr/local/var/postgres start'
 alias wnt='cd ~/project/'
 alias vi='vim'
+alias vimo='vimobox local open'
 
 if uname -r | grep 'micro' >/dev/null
   set -x PATH $HOME/.yarn/bin $PATH
@@ -37,6 +38,7 @@ set -x PATH $HOME/.rbenv/versions/2.7.4/bin  $PATH
 set -x PATH $HOME/.nodenv/bin $PATH
 set -x PATH $HOME/go/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
+set -x PATH $HOME/.deno/bin $PATH
 eval (nodenv init - | source)
 rbenv init - fish | source
 status --is-interactive; and source (rbenv init -|psub)
