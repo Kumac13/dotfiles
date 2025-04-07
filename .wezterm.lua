e29c8eecf309e9bd374c8945f124f41eddbd3c31
Kumac13 <foohilack@gmail.com>
2025-04-07 13:02:10 +0900

    feat: add setting when open new tab

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,4 +7,14 @@ config.font_size = 16.0
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 
+config.keys = {
+  {
+    key = 't',
+    mods = 'CMD',
+    action = wezterm.action.SpawnCommandInNewTab {
+      args = { "/opt/homebrew/bin/tmux", "new-session" },
+    },
+  },
+}
+
 return config
